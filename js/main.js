56dc2f299e40e17808711bb62bd6ab88bf93bf52
kechicheb <93807375+kechicheb@users.noreply.github.com>
2022-09-13 12:27:40 +0200

    Animate Width On Scrolling Stats

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 // Animate Width On Scrolling progress
-const section = document.querySelector("#our-skills");
-const progress = document.querySelectorAll(
-  ".our-skills .prog > span"
-);
-console.log(progress);
+const ourSkills = document.querySelector("#our-skills");
+const progress = document.querySelectorAll(".our-skills .prog > span");
 window.addEventListener("scroll", function () {
   const scrollY = window.scrollY;
-  if (scrollY >= section.offsetTop && scrollY < section.offsetHeight + section.offsetTop) {
+  if (
+    scrollY >= ourSkills.offsetTop &&
+    scrollY < ourSkills.offsetHeight + ourSkills.offsetTop
+  ) {
     progress.forEach((e) => {
       e.style.width = `${e.dataset.width}`;
     });
   }
 });
+
+// Animate Width On Scrolling Stats
+const stats = document.querySelector("#stats");
+const pCount = document.querySelectorAll(
+  " .stats .container .cards .card p:first-of-type"
+);
+let started = true;
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= stats.offsetTop) {
+    if (started) {
+      pCount.forEach((e) => startCount(e));
+    }
+    started = false;
+  }
+});
+function startCount(e) {
+  let count = e.dataset.count;
+  let setCount = setInterval(() => {
+    e.textContent++;
+    if (e.textContent == count) {
+      clearInterval(setCount);
+    }
+  }, 2000 / count);
+}
